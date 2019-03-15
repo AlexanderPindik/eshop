9417250ec0d467b93558ee6230d2adc955ce7357
AlexanderPindik <alexander.pindik@nevafilm.ru>
2019-03-15 16:40:24 +0300

    переведены на русский все формы почтовых отправлений

--- debug.js
+++ debug.js
@@ -6,8 +6,8 @@
 
 const options = {};
 
-// options.ip = '127.0.0.1';
-// options.port = parseInt(process.argv[2]);
+ options.ip = '127.0.0.1';
+ options.port = 80 ;//parseInt(process.argv[2]);
 // options.config = { name: 'Total.js' };
 // options.sleep = 3000;
 // options.inspector = 9229;
